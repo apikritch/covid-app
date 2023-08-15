c4efa8b71d5b21be8a2b79dcce3901c2ea7cf4da
Apikritch Rattanapisankul <apikritch.r@gmail.com>
2023-08-16 01:30:49 +1000

    setup project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {},
+    screens: {
+      "2xs": "320px",
+      xs: "375px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1920px",
+    },
+  },
+  plugins: [],
+};
